69532e09fac87a9d7a3a313124c9cf1fd245c5cb
MSI\pow <chatchawalwas@gosoft.co.th>
2023-12-06 02:22:56 +0700

    ใ

--- src/pages/Home/componentPage/tableHome.js
+++ src/pages/Home/componentPage/tableHome.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   Table,
   TableBody,
@@ -10,68 +11,22 @@ import {
 } from "@mui/material";
 
 const MyTable = () => {
-  const rows = [
-    {
-      id: 1,
-      Ename: "ฟุตบอล",
-      item: 25,
-      type: "New York",
-      borrower: "John Doe",
-      borrowDate: "2023-01-01",
-      returnDate: "2023-01-10",
-      status: "Returned",
-    },
-    {
-      id: 1,
-      Ename: "ฟุตบอล",
-      item: 25,
-      type: "New York",
-      borrower: "John Doe",
-      borrowDate: "2023-01-01",
-      returnDate: "2023-01-10",
-      status: "Returned",
-    },
-    {
-      id: 1,
-      Ename: "ฟุตบอล",
-      item: 25,
-      type: "New York",
-      borrower: "John Doe",
-      borrowDate: "2023-01-01",
-      returnDate: "2023-01-10",
-      status: "Returned",
-    },
-    {
-      id: 1,
-      Ename: "ฟุตบอล",
-      item: 25,
-      type: "New York",
-      borrower: "John Doe",
-      borrowDate: "2023-01-01",
-      returnDate: "2023-01-10",
-      status: "Returned",
-    },
-    {
-      id: 1,
-      Ename: "ฟุตบอล",
-      item: 25,
-      type: "New York",
-      borrower: "John Doe",
-      borrowDate: "2023-01-01",
-      returnDate: "2023-01-10",
-      status: "Returned",
-    },
-    {
-      id: 1,
-      Ename: "ฟุตบอล",
-      item: 25,
-      type: "New York",
-      borrower: "John Doe",
-      borrowDate: "2023-01-01",
-      returnDate: "2023-01-10",
-      status: "Returned",
-    },
-  ];
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/home/management"
+        );
+        setRows(response.data); // setRows เพื่ออัพเดท state rows
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []); // ในที่นี้ใส่ [] เพื่อให้ useEffect ทำงานเพียงครั้งเดียวเมื่อ component ถูก mount
 
   return (
     <TableContainer component={Paper}>
@@ -83,7 +38,7 @@ const MyTable = () => {
             <TableCell>ชื่ออุปกรณ์</TableCell>
             <TableCell>จำนวน</TableCell>
             <TableCell>ประเภท</TableCell>
-            <TableCell>ผุ้ยืม</TableCell>
+            <TableCell>ผู้ยืม</TableCell>
             <TableCell>วันที่ยืม</TableCell>
             <TableCell>วันที่คืน</TableCell>
             <TableCell>สถานะ</TableCell>
@@ -93,12 +48,12 @@ const MyTable = () => {
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell>{row.id}</TableCell>
-              <TableCell>{row.Ename}</TableCell>
-              <TableCell>{row.item}</TableCell>
-              <TableCell>{row.type}</TableCell>
-              <TableCell>{row.borrower}</TableCell>
-              <TableCell>{row.borrowDate}</TableCell>
-              <TableCell>{row.returnDate}</TableCell>
+              <TableCell>{row.equipment_name}</TableCell>
+              <TableCell>{row.quantity}</TableCell>
+              <TableCell>{row.equipment_type}</TableCell>
+              <TableCell>{row.borrower_name}</TableCell>
+              <TableCell>{row.loan_date}</TableCell>
+              <TableCell>{row.return_date}</TableCell>
               <TableCell>{row.status}</TableCell>
             </TableRow>
           ))}
