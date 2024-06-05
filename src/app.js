b2f6340791504eb1b15e9781c798c9d150431ba9
metharae <metharae@gosoft.co.th>
2024-06-06 05:51:24 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home.js';
-import Manage from './pages/LoanDetails/Manage.js';
-import EquipmentMan from './pages/EquitmentSport/Sport.js';
-import EntertainmentMan from './pages/EquitmentRecreational/recreational.js';
-import UserMan from './pages/UserMan/User.js';
-import Report from './pages/report/Report.js';
-import AddpageEq from './pages/EquitmentSport/supPage/addPage.js';
-import Emailsener from './pages/EmailJs/rotueTheme.js';
-import Borrower from './pages/Borrower/index.js';
-import MainPageUser from './pages/MainPageUser/mainPage.js'
-import ReturnTo from './pages/ReturnTo/return.js'
-import QRgen from './pages/ReturnTo/QRcodeGen.js'
-<<<<<<< HEAD
-import Adminsubmit from './pages/Borrower/components/adminEv.js'
-=======
-import Submit from './pages/Submit/Submit.js';
->>>>>>> b04d6ba59c726fc00b4a1df685304c20af3b505b
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import Manage from "./pages/LoanDetails/Manage.js";
+import EquipmentMan from "./pages/EquitmentSport/Sport.js";
+import EntertainmentMan from "./pages/EquitmentRecreational/recreational.js";
+import UserMan from "./pages/UserMan/User.js";
+import Report from "./pages/report/Report.js";
+import AddpageEq from "./pages/EquitmentSport/supPage/addPage.js";
+import Emailsener from "./pages/EmailJs/rotueTheme.js";
+import Borrower from "./pages/Borrower/index.js";
+import MainPageUser from "./pages/MainPageUser/mainPage.js";
+import ReturnTo from "./pages/ReturnTo/return.js";
+import QRgen from "./pages/ReturnTo/QRcodeGen.js";
+import Adminsubmit from "./pages/Borrower/components/adminEv.js";
+import Submit from "./pages/Submit/Submit.js";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/borrower/:Ename/:Etype/:desired_quantity" element={<Borrower />} />
+        <Route
+          path="/borrower/:Ename/:Etype/:desired_quantity"
+          element={<Borrower />}
+        />
         <Route path="/1" element={<Manage />} />
         <Route path="/2" element={<EquipmentMan />} />
         <Route path="/2/add" element={<AddpageEq />} />
@@ -34,12 +34,8 @@ const App = () => {
         <Route path="/MainPage" element={<MainPageUser />} />
         <Route path="/qr" element={<QRgen />} />
         <Route path="/return" element={<ReturnTo />} />
-<<<<<<< HEAD
         <Route path="/submit/:equipment_name/:id" element={<Adminsubmit />} />
-=======
         <Route path="/submit" element={<Submit />} />
-
->>>>>>> b04d6ba59c726fc00b4a1df685304c20af3b505b
       </Routes>
     </Router>
   );
