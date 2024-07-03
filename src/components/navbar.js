c4b4cbbb5e0136e65dc378416e75b6e006a0e929
chatchawal1 <powkub010@gmail.com>
2024-07-03 21:29:32 +0700

    ..

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,24 @@
-// Navbar.js
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Avatar,
-  Button,
-} from "@mui/material";
+import { AppBar, Toolbar, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { styled } from "@mui/system";
 
-const WhiteAppBar = styled(AppBar)({
-  backgroundColor: "#fff",
+const CustomAppBar = styled(AppBar)({
+  backgroundColor: "#333399", // Change the background color to #333399
   boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
   height: 40,
 });
 
 const Navbar = ({ isOpen, onLogout, onToggleSidebar }) => {
   return (
-    <WhiteAppBar position="sticky" elevation={1}>
+    <CustomAppBar position="sticky" elevation={1}>
       <Toolbar>
         <IconButton sx={{ zIndex: 999, ml: 2, mt: -3 }}>
-          <MenuIcon onClick={onToggleSidebar} />
+          <MenuIcon style={{ color: 'white' }} onClick={onToggleSidebar} />
         </IconButton>
         <div style={{ flexGrow: 1 }}></div>
-        
-        
       </Toolbar>
-    </WhiteAppBar>
+    </CustomAppBar>
   );
 };
 
