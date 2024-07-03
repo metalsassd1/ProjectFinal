9cebd5b0c9f9a3da9ba8e3006c0fa391f7ca336a
metharae <metharae@gosoft.co.th>
2024-07-03 21:54:13 +0700

    asd

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,6 +7,9 @@ const CustomAppBar = styled(AppBar)({
   backgroundColor: "#333399", // Change the background color to #333399
   boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
   height: 40,
+  padding: "10px",
+  marginBottom: "10px",
+  marginTop: "-8px",
 });
 
 const Navbar = ({ isOpen, onLogout, onToggleSidebar }) => {
@@ -14,7 +17,9 @@ const Navbar = ({ isOpen, onLogout, onToggleSidebar }) => {
     <CustomAppBar position="sticky" elevation={1}>
       <Toolbar>
         <IconButton sx={{ zIndex: 999, ml: 2, mt: -3 }}>
-          <MenuIcon style={{ color: 'white' }} onClick={onToggleSidebar} />
+          <MenuIcon style={{ color: "white",padding: "10px",
+  marginBottom: "10px",
+  marginTop: "-8px", }} onClick={onToggleSidebar} />
         </IconButton>
         <div style={{ flexGrow: 1 }}></div>
       </Toolbar>
